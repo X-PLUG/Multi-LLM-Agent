968c59680eb8104e26d89b40b29a4fd51b0380c9
shenwzh3 <shenwzh3@mail2.sysu.edu.cn>
2024-02-25 22:45:21 +0800

    fix bug in preprocess stage 1

--- GLPFT/process_data/toolbench/prepro_raw_stage_1.py
+++ GLPFT/process_data/toolbench/prepro_raw_stage_1.py
@@ -27,32 +27,33 @@ for train_set in ["G1_answer", "G2_answer", "G3_answer"]:
         if id not in test_ids:
             with open(f"{args.data_dir}/answer/{train_set}/{file}") as f:
                 d = json.load(f)
-            instruction = d['answer_generation']['query']
-            new_tools = []
-            for t in d['answer_generation']['function']:
-                # tool_name = change_name(standardize(t['api_name'])) + '_for_' + standardize(t['tool_name'])
-                tool_name = t['name']
-                if tool_name != "Finish":
-                    tool_name = tool_name[-64:]
-                    tool_function = t['description'][:256]
-                    tool_input = {}
-                    for arg_name,arg_value in t['parameters']['properties'].items():
-                        arg_type = arg_value['type']
-                        if 'description' in arg_value.keys():
-                            tool_input[arg_name] = arg_type + ', ' + arg_value['description'][-256:]
-                        else:
-                            tool_input[arg_name] = arg_type
-                    # print(json.dumps(t, indent=2))
-                    new_tools.append({
-                        'Name': tool_name,
-                        'function':tool_function,
-                        'input':tool_input
-                    })
-            data.append({
-                'tools':new_tools,
-                'instruction':instruction,
-                'chains':d['answer_generation']['train_messages'][-1]
-            })
+            if "answer_generation" in d.keys() and "train_messages" in d['answer_generation'].keys():
+                instruction = d['answer_generation']['query']
+                new_tools = []
+                for t in d['answer_generation']['function']:
+                    # tool_name = change_name(standardize(t['api_name'])) + '_for_' + standardize(t['tool_name'])
+                    tool_name = t['name']
+                    if tool_name != "Finish":
+                        tool_name = tool_name[-64:]
+                        tool_function = t['description'][:256]
+                        tool_input = {}
+                        for arg_name,arg_value in t['parameters']['properties'].items():
+                            arg_type = arg_value['type']
+                            if 'description' in arg_value.keys():
+                                tool_input[arg_name] = arg_type + ', ' + arg_value['description'][-256:]
+                            else:
+                                tool_input[arg_name] = arg_type
+                        # print(json.dumps(t, indent=2))
+                        new_tools.append({
+                            'Name': tool_name,
+                            'function':tool_function,
+                            'input':tool_input
+                        })
+                data.append({
+                    'tools':new_tools,
+                    'instruction':instruction,
+                    'chains':d['answer_generation']['train_messages'][-1]
+                })
 
     print(train_set,'data processed')
             
